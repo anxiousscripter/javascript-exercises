2a1d621b43ccbc5d0e468cd6e65fe3ab4014df73
anxiousscripter <179371895+anxiousscripter@users.noreply.github.com>
2026-07-24 23:27:38 +0300

    Task Completed: 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+	let fullStr = '';
 
+	if (num < 0) {
+		return 'ERROR';
+	}
+
+	for (let i = 1; i <= num; i++) {
+		fullStr += string;
+	}
+	console.log(fullStr);
+
+	return fullStr;
 };
 
 // Do not edit below this line
