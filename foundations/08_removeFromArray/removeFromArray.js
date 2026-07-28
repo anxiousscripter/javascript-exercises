f34fb6ae208153476a39c5c24989cfa6975c0e8b
anxiousscripter <179371895+anxiousscripter@users.noreply.github.com>
2026-07-28 11:40:36 +0300

    Task Completed: 08_removeFromArray
    Solution includes a separate commented-out solution for test case 1, as
    well as the final solution for all test cases.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+// Test 1 - Solution
+/* const removeFromArray = function (arr, delVal) {
+	let startNum = arr.indexOf(delVal);
+	if (startNum === -1) {
+		console.log(`${delVal} was not found.`);
+	} else {
+		arr.splice(startNum, 1);
+		return arr;
+	}
+}; */
+
+const removeFromArray = function (arr, ...delVal) {
+	for (const val of delVal) {
+		if (arr.includes(val)) {
+			for (let i = arr.length - 1; i >= 0; i--) {
+				if (arr[i] === val) {
+					arr.splice(i, 1);
+				}
+			}
+		} else {
+			console.log(`${val} was not found.`);
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
