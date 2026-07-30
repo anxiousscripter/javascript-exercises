649c5d4d6ef829bd22de473f8d7c6962a245eeb8
anxiousscripter <179371895+anxiousscripter@users.noreply.github.com>
2026-07-30 09:46:41 +0300

    Task Completed: 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreeInFar) {
+	return Number(((degreeInFar - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreeInCel) {
+	return Number((degreeInCel * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
